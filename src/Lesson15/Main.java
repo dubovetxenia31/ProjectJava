e84346fa502305d71eab3cca0dfde3d06551ec35
sadmin <dimaskobsow1508@gmail.com>
2022-05-05 14:05:15 +0300

    test commit2

---
+++ src/Lesson15/Main.java
@@ -0,0 +1,13 @@
+package Lesson15;
+
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Massage info - uraaaaaa");
+
+    }
+}
---
+++ src/Lesson16/Main.java
@@ -0,0 +1,14 @@
+package Lesson16;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
